01136b057c1bc1178e1aa8e1b5c46dcba939f629
Ali Sheykh <sheykha8@gmail.com>
2024-07-07 22:17:38 +0330

    minor changs

--- models/yolo.py
+++ models/yolo.py
@@ -797,6 +797,10 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
             c2 = ch[f] * args[0] ** 2
         elif m is Expand:
             c2 = ch[f] // args[0] ** 2
+        elif m is Decoupled_Detect:
+            args.append([ch[x] for x in f])
+            if isinstance(args[1], int):  # number of anchors
+                args[1] = [list(range(args[1] * 2))] * len(f)
         else:
             c2 = ch[f]
 
